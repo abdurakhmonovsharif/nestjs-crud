eaa068eaf8d3dca586180fff643ec898575e8902
abdurakhmonovsharif <fullstackdeveloper4413@gmail.com>
2024-01-03 01:56:00 +0500

    enalbeCors to

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix("api")
+  app.enableCors()
   await app.listen(8000, "0.0.0.0");
 }
 bootstrap();
